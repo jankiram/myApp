6596b4227744b67bfc1a9ed00657602186b9b325
CHSPLDC\praveen.guraja <praveenkumar.g@callhealth.co.in>
2019-04-05 15:21:12 +0530

    slideer changes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,12 @@
 import {Component, Directive, Input, ViewChild} from '@angular/core';
-import { Slides } from '@ionic/angular';
+import {IonSlides} from '@ionic/angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-	@ViewChild('mySlider', { read: Slides }) slides: Slides;
+	@ViewChild(IonSlides) slides: IonSlides;
 	ionViewDidEnter() {
 	this.slides.startAutoplay();
 	}
